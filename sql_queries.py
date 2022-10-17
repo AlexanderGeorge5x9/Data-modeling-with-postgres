8f427825d41142113a2990439eb1b21c6c90ed91
George Alexander <Alexandergeorge5x9@gmail.com>
2022-10-17 06:53:19 +0200

    second  commit

--- sql_queries.py
+++ sql_queries.py
@@ -9,7 +9,7 @@ time_table_drop = "DROP TABLE IF EXISTS time_table"
 # CREATE TABLES
 
 songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplay_table
-(songplay_id varchar ,
+(songplay_id SERIAL NOT NULL PRIMARY KEY ,
 start_time int,
 user_id int,
 level varchar,
@@ -19,29 +19,29 @@ session_id int,
 location varchar,
 user_agent varchar)""")
 
-user_table_create = ("""CREATE TABLE IF NOT EXISTS user_table
-(user_id int ,
+user_table_create = ("""CREATE TABLE IF NOT EXISTS users
+(user_id int NOT NULL PRIMARY KEY ,
 first_name varchar,
 last_name varchar,
 gender varchar,
 level varchar)""")
 
-song_table_create = ("""CREATE TABLE IF NOT EXISTS song_table
-(song_id varchar ,
+song_table_create = ("""CREATE TABLE IF NOT EXISTS songs
+(song_id varchar NOT NULL PRIMARY KEY ,
 title varchar,
 artist_id varchar,
 year int,
 duration float)""")
 
-artist_table_create = ("""CREATE TABLE IF NOT EXISTS artist_table
-(artist_id varchar ,
+artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists
+(artist_id varchar NOT NULL PRIMARY KEY,
 name varchar,
 location varchar,
 latitude varchar,
 longitude varchar)""")
 
-time_table_create = ("""CREATE TABLE IF NOT EXISTS time_table
-(start_time varchar,
+time_table_create = ("""CREATE TABLE IF NOT EXISTS time
+(start_time TIMESTAMP PRIMARY KEY,
 hour int,
 day int,
 week int,
@@ -51,37 +51,43 @@ weekday int)""")
 
 # INSERT RECORDS
 
-songplay_table_insert = (""""INSERT INTO songplay_table
+songplay_table_insert = (""""INSERT INTO songplays
 (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
 VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+ON CONFLICT(user_id)
+DO UPDATE SET level=EXCLUDED.level;
 """)
 
-user_table_insert = (""" INSERT INTO user_table
+user_table_insert = (""" INSERT INTO users
 (user_id, first_name, last_name, gender, level)
 VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT(user_id)
+DO UPDATE SET level=EXCLUDED.level;
 """)
 
-song_table_insert = ("""INSERT INTO song_table
+song_table_insert = ("""INSERT INTO songs
 (song_id, title, artist_id, year, duration)
 VALUES (%s, %s, %s, %s, %s)
 """)
 
-artist_table_insert = ("""INSERT INTO artist_table
+artist_table_insert = ("""INSERT INTO artists
 (artist_id, name, location, latitude, longitude)
 VALUES (%s, %s, %s, %s, %s)
 """)
 
 
-time_table_insert = ("""INSERT INTO time_table
+time_table_insert = ("""INSERT INTO time
 (start_time, hour, day, week, month, year, weekday)
 VALUES (%s, %s, %s, %s, %s, %s, %s)
 """)
 
 # FIND SONGS
 
-song_select = (""" SELECT * 
-               FROM song_table
-               
+song_select = ("""
+SELECT songs.song_id, songs.artist_id
+FROM songs INNER JOIN artists ON songs.artist_id = artists.artist_id
+WHERE songs.title=%s AND artists.name=%s AND songs.duration=%s
+ORDER BY songs.song_id ;
 """)
 
 # QUERY LISTS
